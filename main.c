c54c487da768d6d42022427da4e6576c7c6fe68f
MihaiMocanu-FMI <mihai.mocanu1@stud.ubbcluj.ro>
2023-03-13 19:41:01 +0200

    working algorithm, needs ui and it's done

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #define NO_LETTERS 26
 
@@ -19,22 +22,59 @@ float computeChiSquared(const float actualFrequencies[NO_LETTERS], const float e
 void readLetterDistribution(const char *filePath, float letterDistribution[NO_LETTERS])
 {
     FILE *pFile = fopen(filePath, "r");
+    if (pFile == NULL)
+    {
+        //Clion build the executable in another folder
+        char newPath[128] = "../";
+        strcat(newPath, filePath);
+        pFile = fopen(newPath, "r");
+
+        if(pFile == NULL)
+            printf("Error opening file");
+    }
 
 
     char line[16];
     fgets(line, 16, pFile);
 
-    while (line != EOF)
+    for (int i = 0; i < NO_LETTERS; ++i)
     {
-        //TODO: Extract letter frequency
-
+        float distribution = atof(line);
+        letterDistribution[i] = distribution;
         fgets(line, 16, pFile);
     }
 }
 
-void computeLetterDistribution(const char text[], float currentLetterDistribution[NO_LETTERS]);
+void computeLetterDistribution(const char text[], float currentLetterDistribution[NO_LETTERS])
+{
+    int frequencyLetters[NO_LETTERS] = {};
+    int totalNumberLetters = 0;
 
-void nextCircularPermutation(float currentPermutation[NO_LETTERS]);
+    while (*text != '\0')
+    {
+        if (isalpha(*text))
+        {
+            char letter = tolower(*text);
+
+            totalNumberLetters++;
+            frequencyLetters[letter - 'a']++;
+        }
+
+        text++;
+    }
+
+    for (int i = 0; i < NO_LETTERS; ++i)
+        currentLetterDistribution[i] = (float)frequencyLetters[i] / totalNumberLetters;
+}
+
+void nextCircularPermutation(float currentPermutation[NO_LETTERS])
+{
+    float lastElement = currentPermutation[NO_LETTERS - 1];
+    for (int i = NO_LETTERS - 1; i >= 1; --i)
+        currentPermutation[i] = currentPermutation[i - 1];
+
+    currentPermutation[0] = lastElement;
+}
 
 
 int findBestEncryptionKey(const char text[])
@@ -64,6 +104,30 @@ int findBestEncryptionKey(const char text[])
     return minKey;
 }
 
+
+void encrypt(char s[], int n)
+{
+    n = n % NO_LETTERS;
+    //a negative number's equivalence class in MOD NO_LETTERS
+    if (n < 0)
+        n = NO_LETTERS + n;
+
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if (isupper(s[i]))
+        {
+            int originOffset = (s[i] - 'A' + n) % NO_LETTERS;
+            s[i] = 'A' + (char)originOffset;
+        }
+        else if( islower(s[i]))
+        {
+            int originOffset = (s[i] - 'a' + n) % NO_LETTERS;
+            s[i] = 'a' + (char)originOffset;
+        }
+    }
+
+}
+
 int main()
 {
     const char englishLetters[NO_LETTERS] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
@@ -71,5 +135,12 @@ int main()
     const float englishFrequencies[NO_LETTERS] = {8.2f, 1.5f, 2.8f, 4.3f, 12.7f, 2.0f, 6.2f, 7.0f, 0.2f, 0.8f, 4.0f,
                                                  2.4f, 6.7f, 7.5f, 1.9f, 0.1f, 6.0f, 2.8f, 9.1f, 2.8f, 1.0f, 2.4f,
                                                  0.2f, 2.0f, 0.1f};
+
+    char text[256] = "Xvznvm Xdkczm! dn vi diozmznodib xjyz dyzv wpo do vxopvggt npxfn hvi";
+    int encryptionKey = findBestEncryptionKey(text);
+
+    encrypt(text, encryptionKey);
+
+    printf("%s", text);
     return 0;
 }
